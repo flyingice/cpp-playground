ced4b44c988676a36ca559d6ee59ce3cd527d244
Yibin YANG <rainforest.vista@gmail.com>
2019-06-13 07:57:51 +0200

    stream writing with buffer

--- exercise/bufferedWrite.cpp
+++ exercise/bufferedWrite.cpp
@@ -1,51 +1,60 @@
 #include <iostream>
-#include <sstream>
+#include <string>
 using namespace std;
 
 class BufferedWrite {
  public:
-  BufferedWrite(ostringstream& oss, int iCapacity) : os(oss), capacity(iCapacity), next(0), buffer(new char[capacity]) {
+  BufferedWrite(ostream& ioStream, int iSize)
+      : stream(ioStream), size(iSize + 1), next(0), sentinel(iSize), buffer(new char[size]), cnt(0) {
   }
 
   virtual ~BufferedWrite() {
+    flush();  // important: flush the remaining elements in the buffer
+    delete[] buffer;
+
+    cout << endl << "flushed " << cnt << " times" << endl;
   }
 
-  void write(const string& iContent);
-  void flush();
+  void write(const string& iStr) {
+    for (size_t i = 0; i < iStr.size(); i++) {
+      if (isFull()) {
+        flush();
+      }
+
+      buffer[next] = iStr[i];
+      next = (next + 1) % size;
+    }
+  }
+
+  void flush() {
+    cnt++;
+    while (!isEmpty()) {
+      sentinel = (sentinel + 1) % size;
+      stream << buffer[sentinel];
+    }
+  }
 
  protected:
-  bool isBufferFull() const {
-    return next >= capacity;
+  bool isFull() const {
+    return next == sentinel;
+  }
+
+  bool isEmpty() const {
+    return (sentinel + 1) % size == next;
   }
 
  private:
-  ostringstream& os;
-  size_t capacity;
-  size_t next;
+  ostream& stream;
+  size_t size;
+  size_t next, sentinel;
   char* buffer;
+
+  int cnt;  // debug only: count of flush() method call
 };
 
-void BufferedWrite::write(const string& iContent) {
-  for (size_t i = 0; i < iContent.size(); i++) {
-    if (isBufferFull()) flush();
-    buffer[next++] = iContent[i];
-  }
-}
-
-void BufferedWrite::flush() {
-  for (size_t i = 0; i < next; i++) {
-    os << buffer[i];
-  }
-
-  next = 0;
-}
-
 int main() {
-  ostringstream os;
-  BufferedWrite aBufferedWrite(os, 5);
+  BufferedWrite aBufferedWrite(cout, 5);
   aBufferedWrite.write("Hello, World!");
-  aBufferedWrite.flush();
-  cout << os.str() << endl;
 
   return 0;
 }
